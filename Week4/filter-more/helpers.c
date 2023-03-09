f48504b7e1490052c45735dd85ba124a52873643
Tazgaru <105459271+Tazgaru@users.noreply.github.com>
2023-03-08 19:48:40 -0600

    Add blur func

--- Week4/filter-more/helpers.c
+++ Week4/filter-more/helpers.c
@@ -40,7 +40,52 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int addRed = 0;
+            int addGreen = 0;
+            int addBlue = 0;
+            // account for edges, since there are no pixels over/behind/below/past them
+            int koffset = 0;
+            int loffset = 0;
+            int kend = 0;
+            int lend = 0;
+
+            if (i == 0) // top edge
+            {
+                koffset = 1;
+            }else if (i == height - 1) // bottom edge
+            {
+                kend = -1;
+            }
+
+            if (j == 0) // left edge
+            {
+                loffset = 1;
+            }else if (j == width - 1) // right edge
+            {
+                lend = -1;
+            }
+            
+            // set pixel's RGB values to average of RGB of 3x3 grid of pixels around them
+            int count = 0;
+            for (int k = -1 + koffset; k < 2 + kend; k++)
+            {
+                for (int l = -1 + loffset; l < 2 + lend; l++)
+                {
+                    addRed += image[i + k][j + l].rgbtRed;
+                    addGreen += image[i + k][j + l].rgbtGreen;
+                    addBlue += image[i + k][j + l].rgbtBlue;
+                    count++;
+                }
+            }
+            image[i][j].rgbtRed = addRed / count;
+            image[i][j].rgbtGreen = addGreen / count;
+            image[i][j].rgbtBlue = addBlue / count;
+        }
+    }
 }
 
 // Detect edges
