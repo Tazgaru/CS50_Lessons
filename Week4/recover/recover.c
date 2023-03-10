ca151b40576faf39344e53a12ebb9dd669933387
Tazgaru <105459271+Tazgaru@users.noreply.github.com>
2023-03-09 20:35:20 -0600

    Recover jpegs from file

--- Week4/recover/recover.c
+++ Week4/recover/recover.c
@@ -1,6 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef __uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    
+    if (argc != 2)
+    {
+        printf("Usage: ./recover.exe file\n");
+        return 1;
+    }
+
+    BYTE bytes_buffer[512];
+    int size_buffer = sizeof(bytes_buffer);
+    int size_item = sizeof(BYTE);
+    int file_count = 0;
+    char file_name[9]; // holds up to name 999.jpeg\0
+
+    FILE *fpin = fopen(argv[1], "r");
+    if (fpin == NULL)
+    {
+        printf("Cannot open file\n");
+        return 1;
+    }
+
+    FILE *fpout = NULL;
+
+    while (fread(bytes_buffer, size_item, size_buffer, fpin) == size_buffer)
+    {
+        if (bytes_buffer[0] == 0xff && bytes_buffer[1] == 0xd8 && bytes_buffer[2] == 0xff)
+        {
+            if (file_count > 0)
+            {
+                fclose(fpout);
+            }
+
+            sprintf(file_name, "%03i.jpeg", file_count);
+            file_count++;
+
+            fpout = fopen(file_name, "a");
+            if (fpout == NULL)
+            {
+                return 3;
+            }
+
+            fwrite(bytes_buffer, size_item, size_buffer, fpout);
+
+        }else if (fpout != NULL)
+        {
+            fwrite(bytes_buffer, size_item, size_buffer, fpout);
+        }
+    }
 }
