175d5708a7063496bc0e3a4db2c5b1964706665e
Tazgaru <105459271+Tazgaru@users.noreply.github.com>
2023-03-09 21:30:25 -0600

    Read header and ensure input is wav

--- Week4/reverse/reverse.c
+++ Week4/reverse/reverse.c
@@ -9,13 +9,14 @@ int get_block_size(WAVHEADER header);
 
 int main(int argc, char *argv[])
 {
+    int header_size = sizeof(WAVHEADER);
+
     // Ensure proper usage
     if (argc != 3)
     {
         printf("Usage: reverse.exe input.wav output.wav\n");
         return 1;
     }
-    
 
     // Open input file for reading
     FILE *fpin = fopen(argv[1], "r");
@@ -24,13 +25,17 @@ int main(int argc, char *argv[])
         printf("Could not open file\n");
         return 1;
     }
-    
 
     // Read header
-    // TODO #3
+    WAVHEADER in_header;
+    fread(&in_header, header_size, 1, fpin);
 
     // Use check_format to ensure WAV format
-    // TODO #4
+    if (!check_format(in_header))
+    {
+        printf("Could not verify input is wav file\n");
+        return 1;
+    }
 
     // Open output file for writing
     // TODO #5
@@ -47,7 +52,11 @@ int main(int argc, char *argv[])
 
 int check_format(WAVHEADER header)
 {
-    // TODO #4
+    if (header.format[0] == 'W' && header.format[1] == 'A' && header.format[2] == 'V' && header.format[3] == 'E')
+    {
+        return 1;
+    }
+
     return 0;
 }
 
