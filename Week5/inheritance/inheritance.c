8866c16856260caa3b93b3e555e2ef0e77c1f876
Tazgaru <105459271+Tazgaru@users.noreply.github.com>
2023-03-11 14:22:14 -0600

    Recursively free all memory allocated

--- Week5/inheritance/inheritance.c
+++ Week5/inheritance/inheritance.c
@@ -80,10 +80,18 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
+    if (p == NULL)
+    {
+        free(p);
+        return;
+    }
 
     // TODO: Free parents recursively
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
 
     // TODO: Free child
+    free(p);
 
 }
 
