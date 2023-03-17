69b8a9af25d655180be5f312b0fb8284e7d5c3c2
tazgaru <105459271+Tazgaru@users.noreply.github.com>
2023-03-17 15:28:01 -0500

    Load dictionary into hash table

--- Week5/speller/dictionary.c
+++ Week5/speller/dictionary.c
@@ -2,6 +2,9 @@
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "dictionary.h"
 
@@ -36,8 +39,43 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+    // allocate memory for word buffer
+    char *str = malloc(LENGTH);
+    if (str == NULL)
+    {
+        printf("Could not allocate memory for word buffer\n");
+        return false;
+    }
+    
+    // open file to read words
+    FILE *fp = fopen(dictionary, "r");
+    if (fp == NULL)
+    {
+        printf("Could not open %s", dictionary);
+        return false;
+    }
+    printf("Opened dictionary...\n");
+
+    // take every word, hash it and put in table
+    while (fgets(str, LENGTH, fp) != NULL)
+    {
+        node *newNode = malloc(sizeof(node));
+        if (newNode == NULL)
+        {
+            printf("Could not allocate memory for new node\n");
+            return false;
+        }
+
+        int hashedWord = hash(str);
+
+        newNode->next = table[hashedWord]; // set new node to point to first node in list (first time is NULL)
+        strcpy(newNode->word, str);
+
+        table[hashedWord] = newNode; // set beginning of list to point to new node (make new node first node in list)
+    }
+    printf("EOF or error encountered. Ended loading dictionary into memory.\n");
+    free(str);
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
@@ -52,4 +90,21 @@ bool unload(void)
 {
     // TODO
     return false;
+}
+
+// ----------------------------------------------------------------- tools
+
+void print_list_array()
+{
+    for (int i = 0; i < N; i++)
+    {
+        node *ptr = table[i];
+
+        while (ptr != NULL)
+        {
+            printf("%s", ptr->word);
+            ptr = ptr->next;
+        }
+        printf("\n");
+    }
 }
